6ee6157b6593cdbc24561dd93d8779252198a5bb
Aarni Koskela <akx@iki.fi>
2019-04-30 11:53:46 +0300

    Reformat files with clang-format

--- charcountmodule.c
+++ charcountmodule.c
@@ -1,70 +1,58 @@
 #define PY_SSIZE_T_CLEAN
-#include <Python.h>
-#include <string.h>
-#include <stdio.h>
 #include "glibc-string.h"
+#include <Python.h>
+#include <stdio.h>
+#include <string.h>
 
 static unsigned int count(const char *str, const char *key) {
   unsigned int n = 0;
-  char * pch = strpbrk (str, key);
-  while (pch != NULL)
-  {
-  	n++;
-    pch = strpbrk (pch + 1, key);
+  char *pch = strpbrk(str, key);
+  while (pch != NULL) {
+    n++;
+    pch = strpbrk(pch + 1, key);
   }
   return n;
 }
 
 static unsigned int gcount(const char *str, const char *key) {
   unsigned int n = 0;
-  char * pch = glibc_strpbrk (str, key);
-  while (pch != NULL)
-  {
-  	n++;
-    pch = glibc_strpbrk (pch + 1, key);
+  char *pch = glibc_strpbrk(str, key);
+  while (pch != NULL) {
+    n++;
+    pch = glibc_strpbrk(pch + 1, key);
   }
   return n;
 }
 
-static PyObject *
-charcount_count(PyObject *self, PyObject *args)
-{
-	const char *str, *key;
-	Py_ssize_t strl, keyl;
-    int mode;
-    
-    if (!PyArg_ParseTuple(args, "s#s#i", &str, &strl, &key, &keyl, &mode)) {
-    	PyErr_SetString(PyExc_RuntimeError, "invalid arguments");
-        return NULL;
-    }
-    //printf("%p %d %p %d\n", str, strl, key, keyl);
-    int n = -1;
-    switch(mode) {
-        case 0:
-            n = count(str, key);
-            break;
-        case 1:
-            n = gcount(str, key);
-            break;
-    }
-    return PyLong_FromLong(n);
+static PyObject *charcount_count(PyObject *self, PyObject *args) {
+  const char *str, *key;
+  Py_ssize_t strl, keyl;
+  int mode;
+
+  if (!PyArg_ParseTuple(args, "s#s#i", &str, &strl, &key, &keyl, &mode)) {
+    PyErr_SetString(PyExc_RuntimeError, "invalid arguments");
+    return NULL;
+  }
+  // printf("%p %d %p %d\n", str, strl, key, keyl);
+  int n = -1;
+  switch (mode) {
+  case 0:
+    n = count(str, key);
+    break;
+  case 1:
+    n = gcount(str, key);
+    break;
+  }
+  return PyLong_FromLong(n);
 }
 
 static PyMethodDef CharCountMethods[] = {
-    {"count", charcount_count, METH_VARARGS, "Count the total occurrences of any s2 characters in s1"},
+    {"count", charcount_count, METH_VARARGS,
+     "Count the total occurrences of any s2 characters in s1"},
     {NULL, NULL, 0, NULL},
 };
 
-static struct PyModuleDef spammodule = {
-    PyModuleDef_HEAD_INIT,
-    "charcount",
-    NULL,
-    -1,
-    CharCountMethods
-};
+static struct PyModuleDef spammodule = {PyModuleDef_HEAD_INIT, "charcount",
+                                        NULL, -1, CharCountMethods};
 
-PyMODINIT_FUNC
-PyInit_charcount(void)
-{
-    return PyModule_Create(&spammodule);
-}
+PyMODINIT_FUNC PyInit_charcount(void) { return PyModule_Create(&spammodule); }
--- glibc-string.h
+++ glibc-string.h
@@ -12,56 +12,57 @@
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
-// adapted from https://github.com/bminor/glibc/blob/fe92a91f1e713ba4951bc6e6162f8803dc76b816/string/strpbrk.c
+// adapted from
+// https://github.com/bminor/glibc/blob/fe92a91f1e713ba4951bc6e6162f8803dc76b816/string/strpbrk.c
 
-#include "string.h"
 #include "libc-pointer-arith.h"
-/* Return the length of the maximum initial segment of S which contains no characters from REJECT.  */
-static size_t glibc_strcspn (const char *str, const char *reject)
-{
-  //if ( (reject[0] == '\0') || (reject[1] == '\0'))
+#include "string.h"
+/* Return the length of the maximum initial segment of S which contains no
+ * characters from REJECT.  */
+static size_t glibc_strcspn(const char *str, const char *reject) {
+  // if ( (reject[0] == '\0') || (reject[1] == '\0'))
   //  return __strchrnul (str, reject [0]) - str;
 
   /* Use multiple small memsets to enable inlining on most targets.  */
   unsigned char table[256];
-  unsigned char *p = memset (table, 0, 64);
-  memset (p + 64, 0, 64);
-  memset (p + 128, 0, 64);
-  memset (p + 192, 0, 64);
+  unsigned char *p = memset(table, 0, 64);
+  memset(p + 64, 0, 64);
+  memset(p + 128, 0, 64);
+  memset(p + 192, 0, 64);
 
-  unsigned char *s = (unsigned char*) reject;
+  unsigned char *s = (unsigned char *)reject;
   unsigned char tmp;
   do
     p[tmp = *s++] = 1;
   while (tmp);
 
-  s = (unsigned char*) str;
-  if (p[s[0]]) return 0;
-  if (p[s[1]]) return 1;
-  if (p[s[2]]) return 2;
-  if (p[s[3]]) return 3;
+  s = (unsigned char *)str;
+  if (p[s[0]])
+    return 0;
+  if (p[s[1]])
+    return 1;
+  if (p[s[2]])
+    return 2;
+  if (p[s[3]])
+    return 3;
 
-  s = (unsigned char *) PTR_ALIGN_DOWN (s, 4);
+  s = (unsigned char *)PTR_ALIGN_DOWN(s, 4);
 
   unsigned int c0, c1, c2, c3;
-  do
-    {
-      s += 4;
-      c0 = p[s[0]];
-      c1 = p[s[1]];
-      c2 = p[s[2]];
-      c3 = p[s[3]];
-    }
-  while ((c0 | c1 | c2 | c3) == 0);
+  do {
+    s += 4;
+    c0 = p[s[0]];
+    c1 = p[s[1]];
+    c2 = p[s[2]];
+    c3 = p[s[3]];
+  } while ((c0 | c1 | c2 | c3) == 0);
 
-  size_t count = s - (unsigned char *) str;
+  size_t count = s - (unsigned char *)str;
   return (c0 | c1) != 0 ? count - c0 + 1 : count - c2 + 3;
 }
 
-
 /* Find the first occurrence in S of any character in ACCEPT.  */
-static char * glibc_strpbrk (const char *s, const char *accept)
-{
-  s += glibc_strcspn (s, accept);
+static char *glibc_strpbrk(const char *s, const char *accept) {
+  s += glibc_strcspn(s, accept);
   return *s ? (char *)s : NULL;
 }
